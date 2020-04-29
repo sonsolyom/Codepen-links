756f15cebf55f307c5939d2f667730e016d92fdc
sonsolyom <nagydsolyom@users.noreply.github.com>
2020-04-29 18:37:50 +0200

    adding more jQuery
    getting the hang of it

--- lists-and-texts/lists-and-texts/dist/script.js
+++ lists-and-texts/lists-and-texts/dist/script.js
@@ -11,3 +11,14 @@ $.each(names, function(index) {
 $('#query-list').css('list-style-type', 'square');
 $("ul li:nth-child(10)").css('font-weight', 'bold');
 
+let additionalBlock = {
+    title: "Added with javascript",
+    text: "This block was added using JavaScript's jQuery library. How awesome!"
+  };
+
+$('#main').append('<div id="new-block"></div');
+$('#new-block').append('<h3 id="new-heading"></h3>');
+$('#new-block').append('<p id="new-paragraph"></p>');
+
+$('#new-heading').text(additionalBlock.title);
+$('#new-paragraph').text(additionalBlock.text);
